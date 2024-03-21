1e748af0bbacfbee74d501f092e73996fdb11bd4
Anusha Kashif <160410652+Anushakashif@users.noreply.github.com>
2024-03-21 14:56:24 -0700

    Add files via upload

---
+++ Q6.js
@@ -0,0 +1,6 @@
+"use strict";
+//  5. Stripping Names : Store a person's name, and include some whitespace characters at the begginning and end of the name. Make sure you use each 
+// character combination, "\t" and "\n" , at  least once . Print the name once, so the whitespace around the name is displayed. Then print the name after strippng the white spaces.
+console.log("AnushaKashif");
+console.log("Anusha\tKashif");
+console.log("Anusha\nKashif");
